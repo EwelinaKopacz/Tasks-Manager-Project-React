12b8d508a01ed0772e79c296d8e8b98a2a8d0738
Ewelina <ewelina@ekopacz.pl>
2022-03-02 23:08:34 +0100

    added first part - add task to API

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -10,8 +10,59 @@ class TasksManager extends React.Component {
         console.log( tasks)
     }
 
+    changeHandler = e => {
+        const {name,value} = e.target;
+        this.setState({
+            [name]:value
+        });
+    }
+
+    submitHandler = e => {
+        e.preventDefault();
+
+        const{task} = this.state;
+        if(task){
+        const requestOptions = {
+            method:'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name:task,time:0,isRunning:"false",isDone:"false",isRemove:"false" })
+        }
+        fetch('http://localhost:3005/data',requestOptions)
+            .then(response => {return response.json()})
+            .then(data => this.addTaskToState(data))
+            .catch(error => console.error(error))
+            .finally(() => {
+                this.setState({task:''})
+            });
+        }
+        else{
+            alert('If you want to add new task, please write a task')
+        }
+    }
+
+    addTaskToState(data){
+        const{name,id} = data;
+        const newTask = {
+            name:name,
+            id:id,
+        }
+        this.setState({
+            tasks:[...this.state.tasks,newTask]
+        })
+    }
+
+
     render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
+        return (
+            <section>
+                <h1 onClick={ this.onClick }>TasksManager</h1>
+                <h2>Add New Task:</h2>
+                    <form onSubmit={this.submitHandler}>
+                        <input name="task" onChange={this.changeHandler} value={this.state.task} />
+                        <input type="submit" value="ADD"/>
+                    </form>
+            </section>
+        )
     }
 }
 
