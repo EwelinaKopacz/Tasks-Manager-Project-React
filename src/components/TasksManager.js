13f6f448fd31a74957392baa1d746da610b1e522
Ewelina <ewelina@ekopacz.pl>
2022-03-04 12:07:49 +0100

    added functions to buttons

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -2,16 +2,7 @@ import React from 'react';
 
 class TasksManager extends React.Component {
     state = {
-        tasks: [
-            {
-            name: "salads",
-            time: 0,
-            isRunning: "false",
-            isDone: "false",
-            isRemove: "false",
-            id: 1
-            }
-        ],
+        tasks: [],
     }
 
     onClick = () => {
@@ -34,7 +25,7 @@ class TasksManager extends React.Component {
         const requestOptions = {
             method:'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name:task,time:0,isRunning:"false",isDone:"false",isRemove:"false" })
+            body: JSON.stringify({ name:task,time:0,isRunning:false,isDone:false,isRemove:false })
         }
         fetch('http://localhost:3005/data',requestOptions)
             .then(response => {return response.json()})
@@ -57,33 +48,42 @@ class TasksManager extends React.Component {
 
     renderTasks(){
         const {tasks} = this.state;
-        return tasks.map(item => {
+        const newList = tasks.filter(task => task.isRemove === false)
+
+        return  newList.map(item => {
             return(
                 <div>
                     <header >{item.name.toUpperCase()}</header>
                     <div>Czas wykonania: {item.time} s.</div>
                     <footer>
-                        <button onClick={this.timeHandler(item)}>{!item.isRunning ? 'START' : 'STOP'}</button>
-                        <button>FINISH</button>
-                        <button disabled="true">REMOVE</button>
+                        {/* <button onClick={() => this.clickStartStop(item)}>{item.isRunning ? 'STOP' : 'START'}</button> */}
+                        <button onClick={() => this.timeHandlerStart(item.id)}>START</button>
+                        <button onClick={() => this.timeHandlerStop(item.id)}>STOP</button>
+                        <button onClick={() => this.finishTaskHandler(item.id)}>{item.isDone ? 'COMPLETED' : 'FINISH'}</button>
+                        <button onClick={() => this.deleteTaskHandler(item.id)} disabled={this.setDisabledStatus(item)}>REMOVE</button>
                     </footer>
                 </div>
             )
         });
     }
+    // NIE CHCE DZIAŁAĆ: WYSWIETLA SIE START, 1 KLIK - WYSWIETLA SIE STOP - 2 KLIK DOPIERO ZLICZA I NIE ZATRZYMUJE SIE NA STOP
+    // clickStartStop = item => {
+    //     const {isRunning} = item;
+    //     item.isRunning ? this.timeHandlerStart(item.id) : this.timeHandlerStop(item.id);
+    // }
 
-    timeHandler = item =>{
-        console.log(item.id);
+    timeHandlerStart = id =>{
+        console.log(id);
         this.idTimer = setInterval(() => {
-            this.incrementTime(item.id);
+            this.incrementTime(id);
           },1000);
     }
 
-    incrementTime(id) {
+    incrementTime (id) {
         this.setState(state => {
             const newTasks = state.tasks.map(task => {
                 if(task.id === id) {
-                    return {...task, time: task.time + 1}
+                    return {...task, time: task.time + 1, isRunning: true}
                 }
                 return task;
             });
@@ -93,10 +93,45 @@ class TasksManager extends React.Component {
         });
     }
 
-    // stopTimer = item => {
-    //     clearInterval(this.idTimer);
-    // }
+    timeHandlerStop = id => {
+        clearInterval(this.idTimer);
 
+        this.setState((prevState) => ({
+            tasks: prevState.tasks.map(task => {
+                if(task.id === id){
+                    return {...task,isRunning:!task.isRunning}
+                }
+                return task;
+            })
+        }))
+    }
+
+    finishTaskHandler = id => {
+        clearInterval(this.idTimer);
+        this.setState((prevState) => ({
+            tasks: prevState.tasks.map(task => {
+                if((task.id === id) || ((task.isRunning))){
+                    return {...task,isDone:!task.isDone,isRunning:false}
+                }
+                return task;
+            })
+        }))
+    }
+    
+    setDisabledStatus = item => {
+        return item.isDone ? false : true;
+    }
+
+    deleteTaskHandler = id => {
+        this.setState((prevState) => ({
+            tasks: prevState.tasks.map(task => {
+                if(task.id === id) {
+                    return {...task,isRemove:!task.isRemove}
+                }
+                return task;
+            })
+        }))
+    }
 
     render() {
         return (
